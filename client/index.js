11788b698e3a1b39b864e184277125c288516f82
Joseph Cruz <cruzj6@wit.edu>
2017-06-27 20:06:34 -0400

    more cleanup

--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import _get from 'lodash/fp/get';
 
 ReactDOM.render(
-	<div>
-		Hello!
-	</div>,
+	<div>Hello!</div>,
 	document.getElementById('root')
-)
+);
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const { routes } = require('./routes/index');
-const traffic = require('./utils/trafficMiddleware');
+const { routes } = require('./server/routes/index');
+const traffic = require('./server/utils/trafficMiddleware');
 
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http)
+const io = require('socket.io')(http);
 
 app.use(traffic);
+app.use(express.static('dist'));
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
 
@@ -14,11 +15,11 @@ io.on('connection', socket => {
 	console.log('Connected!');
 
 	socket.on('message', msg => {
-		console.log({ msg })
+		console.log({ msg });
 		io.emit('message', `You sent me ${msg}!`);
 	});
 
-	socket.on('disconnect', () => console.log('Disconnected!'))
-})
+	socket.on('disconnect', () => console.log('Disconnected!'));
+});
 
 http.listen(process.env.PORT || 5000);
